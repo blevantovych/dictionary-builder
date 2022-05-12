0efcfd86ab63e221f63e114cac9ddab5517a1a7a
blevantovych <levantovychbogdan@gmail.com>
2022-05-12 20:27:23 +0300

    create dictionary.yml using Transform stream create word_category file

--- reader.js
+++ reader.js
@@ -1,24 +1,58 @@
+const { EOL } = require("os");
 const fs = require("fs");
 const path = require("path");
+const { Transform } = require("stream");
+
+const toYML = new Transform({
+  transform: function (chunk, encoding, next) {
+    const ymlRow = chunk
+      .toString()
+      .split(EOL)
+      .map((row) => {
+        const [word, descr] = row.split(";");
+        return `${word}: ${descr}`;
+      })
+      .join(EOL);
+    next(null, ymlRow);
+  },
+});
 
 const rs = fs.createReadStream(
   path.join(process.cwd(), "data", "import_dict.csv")
 );
 
+const ws = fs.createWriteStream(
+  path.join(process.cwd(), "example", "dictionary.yml")
+);
+
+rs.pipe(toYML).pipe(ws);
+
 const categories = {};
-let ymlData = [];
 rs.on("data", (data) => {
-  console.log(
-    data
-      .toString("utf-8")
-      .split("\n")
-      .map((r) => {
-        const [word, descr, cat] = r.split(";");
-        ymlData.push([word, descr]);
-        const category = categories[cat] || [];
-        category.push(word);
-        categories[cat] = category;
-      })
+  data
+    .toString("utf-8")
+    .split(EOL)
+    .map((r) => {
+      const [word, , cat] = r.split(";");
+      const category = categories[cat] || [];
+      category.push(word);
+      categories[cat] = category;
+    });
+});
+
+rs.on("end", () => {
+  const wordCategoryJsonPath = path.join(
+    process.cwd(),
+    "example",
+    "word_category.json"
   );
-  process.exit();
+  fs.writeFile(
+    wordCategoryJsonPath,
+    JSON.stringify(categories, null, 4),
+    (error) => {
+      if (error) {
+        console.error(`Error when writing to ${wordCategoryJsonPath}`);
+      }
+    }
+  );
 });
