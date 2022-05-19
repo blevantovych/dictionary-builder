3b7bb6d0c3d5028b2c6c275e176235cbd74a0153
blevantovych <levantovychbogdan@gmail.com>
2022-05-19 22:53:24 +0300

    child processes pool manager

---
+++ processPoolManager.js
@@ -0,0 +1,111 @@
+const { fork } = require("child_process");
+const path = require("path");
+
+class ProcessPoolManager {
+  constructor({ workers = 3, filesToImport = [] } = {}) {
+    this.workers = workers;
+    this.filesToImport = filesToImport;
+    this.processes = Array.from({ length: workers }).map(() => ({
+      process: fork("reader.js"),
+      busy: false,
+    }));
+    this.importQueue = [];
+    this.setUpProcesses();
+  }
+
+  setUpProcesses() {
+    this.processes.forEach((cp) => {
+      cp.process.on("message", (msg) => {
+        if (msg === "finished") {
+          cp.busy = false;
+          if (this.importQueue.length) {
+            this.import(this.importQueue.shift());
+          }
+        }
+      });
+
+      cp.process.on("exit", (msg) => {
+        console.log("process exited");
+      });
+
+      cp.process.on("disconnect", (msg) => {
+        console.log("process disconnected");
+      });
+
+      cp.process.on("close", (msg) => {
+        console.log("process closed");
+      });
+
+      cp.process.on("error", (msg) => {
+        console.log("process errored");
+      });
+    });
+  }
+
+  import(file) {
+    const freeProcess = this.processes.find(({ busy }) => !busy);
+
+    if (!freeProcess) {
+      console.log("no free process");
+      this.importQueue.push(file);
+      return;
+    }
+
+    const categoryPath = path.join(
+      process.cwd(),
+      "example",
+      `word_category_worker_${path.basename(file)}.json`
+    );
+    const dictionaryPath = path.join(
+      process.cwd(),
+      "example",
+      `dictionary_worker_${path.basename(file)}.yml`
+    );
+
+    freeProcess.busy = true;
+    freeProcess.process.send(
+      JSON.stringify({
+        command: "start",
+        importFrom: file,
+        categoryPath,
+        dictionaryPath,
+      })
+    );
+  }
+
+  run() {
+    this.filesToImport.forEach((file) => {
+      this.import(file);
+    });
+  }
+}
+
+module.exports = { ProcessPoolManager };
+
+const poolManager = new ProcessPoolManager({
+  workers: 1,
+  filesToImport: [
+    path.join(process.cwd(), "data", "import_dict.csv"),
+    path.join(process.cwd(), "data", "import_dict_1.csv"),
+    path.join(process.cwd(), "data", "import_dict_2.csv"),
+    path.join(process.cwd(), "data", "import_dict_3.csv"),
+  ],
+});
+
+poolManager.run();
+
+// setTimeout(() => {
+//   poolManager.import(path.join(process.cwd(), "data", "import_dict_3.csv"));
+// }, 1000);
+
+// setTimeout(() => {
+//   poolManager.import(path.join(process.cwd(), "data", "import_dict_4.csv"));
+// }, 1500);
+
+// setTimeout(() => {
+//   poolManager.import(path.join(process.cwd(), "data", "import_dict_5.csv"));
+// }, 2000);
+
+// setTimeout(() => {
+//   poolManager.import(path.join(process.cwd(), "data", "import_dict_6.csv"));
+// }, 2000);
--- reader.js
+++ reader.js
@@ -1,24 +1,55 @@
+const { EOL } = require("os");
 const fs = require("fs");
-const path = require("path");
+const { Transform, pipeline } = require("stream");
 
-const rs = fs.createReadStream(
-  path.join(process.cwd(), "data", "import_dict.csv")
-);
+const toYML = new Transform({
+  transform: function (chunk, encoding, next) {
+    const ymlRow = chunk
+      .toString()
+      .split(EOL)
+      .map((row) => {
+        const [word, descr] = row.split(";");
+        return `${word}: ${descr}`;
+      })
+      .join(EOL);
+    next(null, ymlRow);
+  },
+});
 
-const categories = {};
-let ymlData = [];
-rs.on("data", (data) => {
-  console.log(
+process.on("message", (msg) => {
+  const { command, categoryPath, dictionaryPath, importFrom } = JSON.parse(msg);
+  if (command === "start") {
+    console.log("got start command importing ", importFrom);
+    process.send("started");
+    importFile({ categoryPath, dictionaryPath, importFrom });
+  }
+});
+
+const importFile = ({ categoryPath, dictionaryPath, importFrom }) => {
+  const rs = fs.createReadStream(importFrom);
+  const ws = fs.createWriteStream(dictionaryPath);
+
+  rs.pipe(toYML).pipe(ws);
+
+  const categories = {};
+  rs.on("data", (data) => {
     data
       .toString("utf-8")
-      .split("\n")
+      .split(EOL)
       .map((r) => {
-        const [word, descr, cat] = r.split(";");
-        ymlData.push([word, descr]);
+        const [word, , cat] = r.split(";");
         const category = categories[cat] || [];
         category.push(word);
         categories[cat] = category;
-      })
-  );
-  process.exit();
-});
+      });
+  });
+
+  rs.on("end", () => {
+    fs.writeFile(categoryPath, JSON.stringify(categories, null, 4), (error) => {
+      if (error) {
+        console.error(`Error when writing to ${categoryPath}`);
+      }
+      process.send("finished");
+    });
+  });
+};
